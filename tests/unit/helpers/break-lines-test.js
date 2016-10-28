20f46908ef6befc4a3d59618de9188e55aaca48e
Marta Sanchez <sanchez.marta@gmail.com>
2016-10-28 12:49:37 -0700

    create a new helper class to escape p html tags from the firebase json file

---
+++ tests/unit/helpers/break-lines-test.js
@@ -0,0 +1,12 @@
+
+import { breakLines } from 'questions-project/helpers/break-lines';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | break lines');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = breakLines([42]);
+  assert.ok(result);
+});
+
