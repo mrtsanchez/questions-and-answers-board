d3cdecb7030ef15f27f1f99f02b63f872f8ff810
Marta Sanchez <sanchez.marta@gmail.com>
2016-11-06 20:59:28 -0800

    sort answers by number of upvotes, implementing one computed property

--- app/components/question-expand.js
+++ app/components/question-expand.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['votes:desc'],
+  sortedAnswers: Ember.computed.sort('question.answers', 'sortBy'),
+
   actions: {
     upvote(answer){
       this.sendAction('upvote', answer);
