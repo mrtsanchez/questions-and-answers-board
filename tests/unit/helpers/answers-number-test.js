b3910a2001afc71b2e779cd9c127f8e00cb4cf8c
Marta Sanchez <sanchez.marta@gmail.com>
2016-11-07 16:36:20 -0800

    display the favorite questions of the user in a side bar

---
+++ tests/unit/helpers/answers-number-test.js
@@ -0,0 +1,12 @@
+
+import { answersNumber } from 'questions-project/helpers/answers-number';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | answers number');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = answersNumber([42]);
+  assert.ok(result);
+});
+
