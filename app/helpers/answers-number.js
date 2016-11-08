d9aba8fbe2722c08a98960b3fa48ee2bbb21b131
Marta Sanchez <sanchez.marta@gmail.com>
2016-11-08 08:57:01 -0800

    display the number of answers in the fav questions list

--- app/helpers/answers-number.js
+++ app/helpers/answers-number.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
-export function answersNumber(params/*, hash*/) {
-  return params;
+export function answersNumber(params) {
+  var question = params[0];
+  var answersTotal = question.get('answers').get('length');
+  if(answersTotal >= 2) {
+    return answersTotal + Ember.String.htmlSafe(' answers');
+  } else if (answersTotal = 0) {
+    return Ember.String.htmlSafe('no answers');
+  } else {
+    return Ember.String.htmlSafe('1 answer');
+  }
 }
 
 export default Ember.Helper.helper(answersNumber);
